e420db78e82a50a511c42092106339a7cab6f40c
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-02-20 13:48:58 +0100

    Initial Commit

---
+++ SwiftUISideDrawerMenu/SwiftUISideDrawerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISideDrawerMenuApp.swift
+//  SwiftUISideDrawerMenu
+//
+//  Created by Kaori Persson on 2023-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISideDrawerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
